2c93083edbe55b32ee0cdf676c9f3c6353fa3fff
Bluice Zhen <bluice.zhen@gmail.com>
2017-06-28 16:37:25 +0800

    release 0.0.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Watermelon-Qiniu
+# Watermelon Qiniu
 西瓜七牛 - 一个开发者友好的七牛云JS SDK
+
+**实现的功能：**
+
+- 文件上传
+
+**TODO List:**
+
+- 获取文件（非公开）
---
+++ main.js
@@ -0,0 +1,25 @@
+export default {
+    upload (file, key, token) {
+        let xhr = new XMLHttpRequest();
+        // TODO: 支持使用不同的地址
+        xhr.open('POST', 'http://up-z2.qiniu.com', true);
+        let formData = new FormData();
+        formData.append('token', token);
+        formData.append('key', key);
+        formData.append('file', file);
+
+        let p = new Promise(function (resolve, reject) {
+            xhr.onreadystatechange = function (response) {
+                if (xhr.readyState === 4 && xhr.status === 200 && xhr.responseText !== '') {
+                    let blkRet = JSON.parse(xhr.responseText);
+                    resolve(blkRet, response);
+                } else if (xhr.readyState === 4) {
+                    reject('err', response);
+                }
+            };
+            xhr.send(formData);
+        });
+
+        return p;
+    }
+};
